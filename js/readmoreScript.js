c344b73fbd7a908b964dd588b32e2b810472617a
WinglessKing <128549723+WinglessKing@users.noreply.github.com>
2023-07-04 18:26:01 -0400

    Update readmoreScript.js

--- js/readmoreScript.js
+++ js/readmoreScript.js
@@ -1,14 +1,14 @@
 function myFunction() {
-  var colon = document.getElementById("colon");
+  var arrow = document.getElementById("arrow");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("readMoreBtn");
 
-  if (colon.style.display === "none") {
-    colon.style.display = "inline";
+  if (arrow.style.display === "none") {
+    arrow.style.display = "inline";
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
   } else {
-    colon.style.display = "none";
+    arrow.style.display = "none";
     btnText.innerHTML = "Read less";
     moreText.style.display = "inline";
   }
